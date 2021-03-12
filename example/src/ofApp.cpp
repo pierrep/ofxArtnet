b297801ea46f8498867ce78e5325be20d86ce2da
Pierre Proske <pierre@sensoryempire.com>
2021-03-12 11:42:28 +1100

    updated example

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,39 +1,60 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
+void ofApp::setup(){
     //at first you must specify the Ip address of this machine
-    artnet.setup("10.0.0.4"); //make sure the firewall is deactivated at this point
+    artnet.setup("192.168.0.5"); //make sure the firewall is deactivated at this point
     
     ofSetFrameRate(40);
     fbo.allocate(512, 1, GL_RGB);
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
-    ofSetWindowTitle(ofToString(ofGetFrameRate(), 2));
-    
-    //create send buffer by ofFbo
-    {
-        fbo.begin();
-        ofClear(0);
-        float colorR = (sin(ofGetElapsedTimeMillis() / 1000.f) / 2.f + 0.5f) * 255.f;
-        float colorG = (sin((ofGetElapsedTimeMillis() / 1000.f) + PI / 3.f) / 2.f + 0.5f) * 255.f;
-        float colorB = (sin((ofGetElapsedTimeMillis() / 1000.f)  + PI * 2.f / 3.f) / 2.f + 0.5f) * 255.f;
-        ofSetColor((int)colorR, (int)colorG, (int)colorB);
-        ofRect(0, 0, 512, 1);
-        fbo.end();
-        fbo.readToPixels(testImage.getPixelsRef());
-    }
+void ofApp::exit()
+{
+    unsigned char buf[512];
+    memset(&buf,0,512);
 
-    //list nodes for sending
-    //with subnet / universe
-//    artnet.sendDmx("10.0.0.149", 0xf, 0xf, testImage.getPixels(), 512);
-    artnet.sendDmx("10.0.0.149", testImage.getPixels(), 512);
+    //turn off lights
+    artnet.sendDmx("192.168.0.101", buf, 512);
+    artnet.sendDmx("192.168.0.102", buf, 512);
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
+void ofApp::update(){
+    ofSetWindowTitle(ofToString(ofGetFrameRate(), 2));
+    
+    //create send buffer by ofFbo
+    fbo.begin();
+    ofClear(0);
+    float colorR = (sin(ofGetElapsedTimeMillis() / 1000.f) / 2.f + 0.5f) * 255.f;
+    float colorG = (sin((ofGetElapsedTimeMillis() / 1000.f) + PI / 3.f) / 2.f + 0.5f) * 255.f;
+    float colorB = (sin((ofGetElapsedTimeMillis() / 1000.f)  + PI * 2.f / 3.f) / 2.f + 0.5f) * 255.f;
+    ofSetColor((int)colorR, (int)colorG, (int)colorB);
+    ofDrawRectangle(0, 0, 512, 1);
+    fbo.end();
+    fbo.readToPixels(testImage.getPixels());
+
+    //with subnet / universe
+    //artnet.sendDmx("10.0.0.149", 0xf, 0xf, testImage.getPixels(), 512);
+    artnet.sendDmx("192.168.0.101", testImage.getPixels().getData(), 512);
+
+    //write directly into send buffer
+    unsigned char buf[512];
+    memset(&buf,0,512);
+    buf[0] = 255;
+    buf[1] = 0;
+    buf[2] = 0;
+    buf[3] = 0;
+    buf[4] = 0;
+    buf[5] = 0;
+    buf[6] = 0;
+
+    artnet.sendDmx("192.168.0.102", buf, 512);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
     ofBackground(0);
     float scalex = ofGetWidth() / fbo.getWidth();
     float scaley = ofGetHeight() / fbo.getHeight();
@@ -42,46 +63,46 @@ void testApp::draw(){
 }
 
 //--------------------------------------------------------------
-void testApp::keyPressed(int key){
+void ofApp::keyPressed(int key){
 
 }
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){
+void ofApp::keyReleased(int key){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y ){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h){
 
 }
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg){
 
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
--- example/src/ofApp.h
+++ example/src/ofApp.h
@@ -3,12 +3,13 @@
 #include "ofMain.h"
 #include "ofxArtnet.h"
 
-class testApp : public ofBaseApp{
+class ofApp : public ofBaseApp{
 
 	public:
 		void setup();
 		void update();
 		void draw();
+        void exit();
 
 		void keyPressed  (int key);
 		void keyReleased(int key);
